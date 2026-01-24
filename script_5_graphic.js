afd86173a169f4a41ea8d347893d191b46e0ab65
14OlaN <34922541+14OlaN@users.noreply.github.com>
2026-01-24 14:24:26 -0500

    revert script_5_graphic.js to original

--- script_5_graphic.js
+++ script_5_graphic.js
@@ -17,10 +17,6 @@ const bgColors = {
     dark: "#00060B"
 };
 
-const stars = [];
-const STAR_FADE = 0.003;     // fade per frame
-const STAR_MIN_ALPHA = 0.1; // 6% floor
-
 const keyboardLayout = {
     // numbers
     "1": [0, 0], "2": [1, 0], "3": [2, 0], "4": [3, 0], "5": [4, 0],
@@ -101,12 +97,6 @@ function drawBackground() {
         innerColor = interpolateColor(bgColors.dark, bgColors.base, t);
     }
 
-    // 1. reset background
-    ctx.globalAlpha = 1;
-    ctx.fillStyle = bgColors.base;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    // 2. draw glow
     const gradient = ctx.createRadialGradient(
         canvas.width / 2,
         canvas.height / 2,
@@ -119,8 +109,10 @@ function drawBackground() {
     gradient.addColorStop(0, innerColor);
     gradient.addColorStop(1, outerColor);
 
+    ctx.globalAlpha = 0.06; // controls fade speed
     ctx.fillStyle = gradient;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.globalAlpha = 1;
 }
 
 // TEXT - QUESTIONS, DATE, TIME
@@ -208,11 +200,8 @@ function drawStarForKey(key) {
     const paddingX = canvas.width * 0.05;
     const paddingY = canvas.height * 0.05;
 
-    const zoneWidth = canvas.width / (maxCols + 1);
-    const zoneHeight = canvas.height / (maxRows + 1);
-
-    const spreadX = zoneWidth * 0.8;
-    const spreadY = zoneHeight * 0.8;
+    const spreadX = canvas.width * 0.25;
+    const spreadY = canvas.height * 0.25;
 
     const x =
         paddingX +
@@ -229,23 +218,7 @@ function drawStarForKey(key) {
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
 
-    stars.push({
-    x,
-    y,
-    alpha: 0.8
-});
-}
-
-function drawStars() {
-    ctx.font = "100 20px 'IBM Plex Mono', monospace";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-
-    for (let s of stars) {
-        s.alpha = Math.max(STAR_MIN_ALPHA, s.alpha - STAR_FADE);
-        ctx.fillStyle = `rgba(255,255,255,${s.alpha})`;
-        ctx.fillText("*", s.x, s.y);
-    }
+    ctx.fillText("*", x, y);
 }
 
 
@@ -295,7 +268,6 @@ document.addEventListener('keypress', updateAnswerString);
 
 function animate() {
     drawBackground();
-    drawStars();
     requestAnimationFrame(animate);
 }
 
